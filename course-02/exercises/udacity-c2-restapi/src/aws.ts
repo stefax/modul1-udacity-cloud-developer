c536cbdf4b543e13f7cb5b1fb37f3c3c897bea8f
Stefan Richter <stefan@modul1.de>
2020-04-23 21:30:16 +0200

    minor fixes in order to deploy, e.g. correct return types, missing env vars and correct package versions

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -2,8 +2,10 @@ import AWS = require('aws-sdk');
 import { config as c } from './config/config';
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
-AWS.config.credentials = credentials;
+if (c.aws_profile !== "DEPLOYED") {
+    var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+    AWS.config.credentials = credentials;
+}
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
