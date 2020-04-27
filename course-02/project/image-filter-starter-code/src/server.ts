5ce8d0672774791531a86740e33c46e45190f74c
Stefan Richter <stefan@modul1.de>
2020-04-27 15:38:34 +0200

    feat: Create endpoint to filter a public image and show the results
    Use some packages for better readability and maintainability.
    - dotenv for better documented environment variables
    - http-status-codes for constant based response codes
    - node-fetch for easy checking whether the remote image exists

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,11 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import HttpStatus from 'http-status-codes';
+import fetch from 'node-fetch';
+import * as fs from 'fs';
+
+require('dotenv').config();
 
 (async () => {
 
@@ -9,34 +14,40 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+  app.get( "/", ( req: Request, res: Response ) => {
+    res.send("try GET /filteredimage?image_url={{URL}}");
   } );
-  
+
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+    let { image_url } = req.query;
+    if (!image_url) {
+      return res.status(HttpStatus.BAD_REQUEST).send('You need to provide the image_url query parameter.');
+    }
+
+    try {
+      const fetchRes = await fetch(image_url, {method: 'HEAD'});
+      if (!fetchRes.ok) {
+        throw 'Fetching image_url failed.';
+      }
+    } catch (error) {
+      return res.status(HttpStatus.NOT_FOUND).send('The image url could not be found. Error: ' + error);
+    }
+
+    const filteredImageFilepath = await filterImageFromURL(image_url);
+    const filteredImageFileContent = fs.readFileSync(filteredImageFilepath);
+
+    deleteLocalFiles([filteredImageFilepath]);
+
+    res.status(HttpStatus.OK).set('Content-Disposition', 'inline').set('Content-type', 'image/jpeg').send(filteredImageFileContent);
+  } );
 
   // Start the Server
   app.listen( port, () => {
